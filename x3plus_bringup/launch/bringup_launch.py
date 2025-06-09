0343b07b9a01ede8ae75e25cfd1daca49c2f407d
cord-burmeister <cord.burmeister@live.de>
2025-06-09 19:59:26 +0200

    Debugging imu sensors

--- x3plus_bringup/launch/bringup_launch.py
+++ x3plus_bringup/launch/bringup_launch.py
@@ -181,6 +181,22 @@ def generate_launch_description():
                           'rviz_config': rviz_config_file}.items())
 
 
+
+    imu_filter_madgwick_cmd = Node(
+            package='imu_filter_madgwick',
+            executable='imu_filter_madgwick_node',
+            name='imu_filter_madgwick',
+            output='screen',
+         parameters=[os.path.join(pkg_location, 'config/imu_filter_param.yaml'), {'use_sim_time': use_sim_time}]
+        )
+
+            # parameters=[{
+            #     'use_mag': False,  # Set True if magnetometer data is used
+            #     'publish_tf': False, # Set True if TF should be published
+            #     'world_frame': 'enu',  # Options: 'enu', 'nwu', 'ned'
+            # }]
+
+
     robot_localization_cmd = Node(
          package='robot_localization',
          executable='ekf_node',
@@ -230,6 +246,7 @@ def generate_launch_description():
 
     # Add the actions to launch all of the navigation nodes
     ld.add_action(rviz_cmd)
+    ld.add_action(imu_filter_madgwick_cmd)
     ld.add_action(robot_localization_cmd)
     ld.add_action(bringup_cmd)
     
