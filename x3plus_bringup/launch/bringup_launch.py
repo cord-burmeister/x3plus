2f4b4b2e6e4f5a4ccb29671e456d09e560f2073c
cord-burmeister <cord.burmeister@live.de>
2025-08-18 20:32:39 +0200

    Adjust the paramter to use nav2

--- x3plus_bringup/launch/bringup_launch.py
+++ x3plus_bringup/launch/bringup_launch.py
@@ -86,7 +86,7 @@ def generate_launch_description():
 #region  Launch configuration variables specific to simulation
     rviz_config_file = LaunchConfiguration('rviz_config_file')
     use_rviz = LaunchConfiguration('use_rviz')
-    use_x3plus = LaunchConfiguration('use_x3plus')
+    use_nav2 = LaunchConfiguration('use_nav2')
 
 #endregion 
 
@@ -165,8 +165,8 @@ def generate_launch_description():
         default_value='True',
         description='Whether to start RVIZ')
 
-    declare_use_x3plus_cmd = DeclareLaunchArgument(
-        'use_x3plus',
+    declare_use_nav2_cmd = DeclareLaunchArgument(
+        'use_nav2',
         default_value='False',
         description='Whether to start the x3plus logic')
 
@@ -226,7 +226,7 @@ def generate_launch_description():
     bringup_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(launch_dir, 'bringup_launch.py')),
-        condition=IfCondition(use_x3plus),
+        condition=IfCondition(use_nav2),
         launch_arguments={'namespace': namespace,
                           'use_namespace': use_namespace,
                           'slam': slam,
@@ -254,7 +254,7 @@ def generate_launch_description():
     ld.add_action(declare_rviz_config_file_cmd)
     ld.add_action(declare_use_robot_state_pub_cmd)
     ld.add_action(declare_use_rviz_cmd)
-    ld.add_action(declare_use_x3plus_cmd)
+    ld.add_action(declare_use_nav2_cmd)
     ld.add_action(declare_map_cmd)
 
     # ld.add_action(declare_robot_name_cmd)
